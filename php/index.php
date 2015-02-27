918622b62b199e883a3fd6ff1e9da16d2355aa09
Anthony Pavaux <a.pavaux@beware.fr>
2015-02-27 17:13:59 +0100

    Add dossier php

---
+++ php/index.php
@@ -0,0 +1,25 @@
+<?php 
+
+
+
+
+
+	function AfficherMessage($message,$type){
+		switch ($type) {
+			case 'info':
+					//Afficher message
+				break;
+			case 'warning':
+					//Afficher message
+				break;
+			case 'error':
+					//Afficher message
+				break;
+			
+			default:
+					//Afficher message
+				break;
+		}
+	}
+
+?>
